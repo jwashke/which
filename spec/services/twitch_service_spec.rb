ec0c45472651e0bb2c03b0d1e9f167fe25aef0c1
Josh Washke <jwashke@icloud.com>
2016-05-26 09:48:51 -0600

    Implement Unit Testing for private methods in TwitchService

--- spec/services/twitch_service_spec.rb
+++ spec/services/twitch_service_spec.rb
@@ -78,4 +78,98 @@ RSpec.describe TwitchService do
       end
     end
   end
+
+  describe "#get_followed" do
+    it "gets the response of the followed streams" do
+      VCR.use_cassette("TwitchService followed_hash") do
+        service = TwitchService.new
+        response = service.get_followed(0)
+
+        expect(response.body).to include("Hearthstone")
+        expect(response.body).to include("AmazHS")
+      end
+    end
+  end
+
+  describe "#get_featured" do
+    it "gets the response of the featured streams" do
+      VCR.use_cassette("TwitchService featured_hash") do
+        service = TwitchService.new
+        response = service.get_featured
+
+        expect(response.body).to include("Deadmau5")
+        expect(response.body).to include("WARHAMMER")
+      end
+    end
+  end
+
+  describe "#get_channel" do
+    it "gets the response of the requested channel" do
+      VCR.use_cassette("TwitchService channel_hash") do
+        service = TwitchService.new
+        response = service.get_channel("Savjz")
+
+        expect(response.body).to include("Hearthstone")
+        expect(response.body).to include("yogg druid")
+      end
+    end
+  end
+
+  describe "#get_streams" do
+    it "returns the response of 25 streams for a given game" do
+      VCR.use_cassette("TwitchService streams_hash") do
+        service = TwitchService.new
+        response = service.get_streams("Overwatch")
+
+        expect(response.body).to include("JoshOG")
+        expect(response.body).to include("Overwatch-OG")
+      end
+    end
+  end
+
+  describe "#get_games" do
+    it "returns the response of the top 12 games" do
+      VCR.use_cassette("TwitchService games_hash") do
+        service = TwitchService.new
+        response = service.get_games
+
+        expect(response.body).to include("Overwatch")
+        expect(response.body).to include("League of Legends")
+        expect(response.body).to include("Hearthstone")
+      end
+    end
+  end
+
+  describe "#parse" do
+    it "parses JSON response and symbolized the names" do
+      VCR.use_cassette("TwitchService games_hash") do
+        service = TwitchService.new
+        response = service.get_games
+        parsed = service.parse(response)
+
+        expect(parsed[:_total]).to eq(1025)
+        expect(parsed.count).to eq(3)
+      end
+    end
+  end
+
+  describe "#remove_whitespace" do
+    it "replaces spaces in a string with '+'" do
+      service = TwitchService.new
+      string = "Hearthstone: Heroes of Warcraft"
+      formatted_string = service.remove_whitespace(string)
+
+      expect(formatted_string).to eq("Hearthstone:+Heroes+of+Warcraft")
+    end
+  end
+end
+
+class TwitchService
+  public :get_followed,
+         :get_featured,
+         :get_channel,
+         :get_streams,
+         :get_games,
+         :parse,
+         :remove_whitespace
 end
