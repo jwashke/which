7d1a1749e11849e4b62782692bfb78de77af6a20
Josh Washke <jwashke@icloud.com>
2016-06-23 23:18:23 -0600

    Minor refactoring

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitch, ENV["TWITCH_CLIENT_ID"], ENV["TWITCH_CLIENT_SECRET"], scope: "user_read user_follows_edit"
+  provider :twitch,
+  ENV["TWITCH_CLIENT_ID"],
+  ENV["TWITCH_CLIENT_SECRET"],
+  scope: "user_read user_follows_edit"
 end
